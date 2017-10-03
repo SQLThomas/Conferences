1406191625dffc9ba58cb98f6cf32a1d7aa8fda5
Thomas <SQLThomas@users.noreply.github.com>
2017-10-03 15:46:31 +0200

    Add files via upload

---
+++ SQLSat666/Dynamic2.R
@@ -0,0 +1,73 @@
+# Dynamic2.R
+# Demo of static vs dynamic SQL statements
+# ©2017 Thomas Hütter, this script is provided as-is for demo and educational use only,
+# without warranty of any kind for any other purposes, so run at your own risk!
+  
+# Connect to our SQL Server using DBI
+# !! We need a Dynamics NAV/Navision database for this part of the demo !!
+library(DBI)
+# conn <- dbConnect(RSQLServer::SQLServer(), server = "localhost", database = 'NavDemo', 
+#                   properties= list(user = 'gast', password = 'Gast2000!'))
+conn <- dbConnect(RSQLServer::SQLServer(), server = "win10sql", database = 'NavDemo', 
+                  properties= list(user = 'gast', password = 'Gast2000!'))
+
+# Multiple companies in our ERP database
+comp <- dbGetQuery(conn, "select Name from NavDemo.dbo.Company", stringsAsFactors = FALSE)
+comp
+
+# (Almost) all tables exist once per company
+tab <- dbGetQuery(conn, "select name from sys.tables where name like '%$Customer'", stringsAsFactors = FALSE)
+tab
+
+# Collect customer names from all companies
+for (tname in tab$name) print(dbGetQuery(conn, 
+    paste0("select Name from NavDemo.dbo.[", tname, ']'), stringsAsFactors = FALSE))
+
+# Extract some sales data
+sales <- dbGetQuery(conn, 
+"SELECT TOP 20 CLE.[Customer No_], CLE.[Posting Date], CLE.[Sales (LCY)]
+FROM NavDemo.dbo.[NorthTank$Cust_ Ledger Entry] CLE
+WHERE (CLE.[Sales (LCY)] <> 0)
+AND (CLE.[Posting Date] BETWEEN '20150101' AND '20151231')", stringsAsFactors = FALSE)
+sales
+
+# Summarize sales per month per post code
+sales <- dbGetQuery(conn, 
+"SELECT CU.[Post Code] As Postcode, Datepart(Month, CLE.[Posting Date]) As Mon, SUM(CLE.[Sales (LCY)]) AS Amount
+FROM NavDemo.dbo.[NorthTank$Customer] CU
+LEFT OUTER JOIN NavDemo.dbo.[NorthTank$Cust_ Ledger Entry] CLE
+  ON CLE.[Customer No_] = CU.No_
+WHERE (CLE.[Sales (LCY)] <> 0)
+  AND (CLE.[Posting Date] BETWEEN '20150101' AND '20151231')
+GROUP BY CU.[Post Code], CLE.[Posting Date]
+ORDER BY CU.[Post Code], CLE.[Posting Date]", stringsAsFactors = FALSE)
+head(sales, 20)
+
+# Use a function to iterate through companies for summarizing sales data
+funcSales <- function(compname) {dbGetQuery(conn, paste0(
+  "SELECT '", compname, "' As Sub, CU.[County] As County, SUM(CLE.[Sales (LCY)]) AS Amount
+  FROM NavDemo.dbo.[", compname, "$Customer] CU
+  LEFT OUTER JOIN NavDemo.dbo.[", compname, "$Cust_ Ledger Entry] CLE
+    ON CLE.[Customer No_] = CU.No_
+  WHERE (CLE.[Sales (LCY)] <> 0)
+    AND (CLE.[Posting Date] BETWEEN '20150101' AND '20151231')
+  GROUP BY CU.[County]"),  
+  as.is = c(TRUE, TRUE, FALSE), stringsAsFactors = FALSE)}
+
+# Show for one company
+funcSales('NorthTank')
+# Call for all companies
+sales <- apply(comp, 1, FUN=funcSales)
+str(sales)
+
+# Convert resulting list to a data frame
+sales.all <- do.call(rbind, sales)
+sales.all$Sub <- factor(sales.all$Sub)
+sales.all$County <- factor(sales.all$County)
+sales.all
+# Save our data, we'll need it later
+save(sales.all, file = "Data/_sales_all.Rdata")
+
+# Cleanup
+dbDisconnect(conn)
+# rm(list = ls())
---
+++ SQLSat666/Shape2.R
@@ -0,0 +1,74 @@
+# Shape2.R
+# Demo of using shapefiles in R
+# ©2017 Thomas Hütter, this script is provided as-is for demo and educational use only,
+# without warranty of any kind for any other purposes, so run at your own risk!
+ 
+# Load needed packages
+library(GISTools)
+library(plyr)
+library(XML)
+
+# Show RColorBrewer palettes
+display.brewer.all()
+
+# Load map data for Europe
+p4str <- CRS('+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0')
+europe <- readShapePoly('data/europe', proj4string=p4str)
+plot(europe)
+
+# Fetch GDP data
+gdpdata <- readHTMLTable('http://statisticstimes.com/economy/countries-by-projected-gdp.php/', 
+           which = 2, stringsAsFactors = FALSE)
+# load('data/_gdpdata.Rdata')
+gdpdata[3:12] <- NULL
+names(gdpdata) <- c('country', 'gdp')
+gdpdata$gdp <- as.numeric(gsub(',', '', gdpdata$gdp))
+gdpdata$country[gdpdata$country == 'Slovak Republic'] <- 'Slovakia'
+gdpdata$country[gdpdata$country == 'FYR Macedonia'] <- 'The former Yugoslav Republic of Macedonia'
+gdpdata$country[gdpdata$country == 'Moldova'] <- 'Republic of Moldova'
+europe@data <- merge(europe@data, gdpdata, by.x='NAME', by.y='country', all.x=TRUE, sort=FALSE)
+europe@data <- europe@data[order(as.integer(as.character(europe@data$SP_ID))),]
+europe$gdp[is.na(europe$gdp)] <- 0
+shades.gdp <- auto.shading(europe$gdp, n = 10, cutter = quantileCuts, cols = brewer.pal(10, "RdYlBu"))
+choropleth(europe, europe$gdp, shades.gdp)
+choro.legend(45, 80, shades.gdp, fmt="%4.0f", title = "GDP (Billion US$)", 
+             cex = 0.75, inset = 0.15)
+title('GDP across Europe')
+
+# Fetch population data
+popdata <- readHTMLTable('http://statisticstimes.com/population/countries-by-population.php/', 
+                     which = 2, stringsAsFactors = FALSE)
+# load('data/_popdata.Rdata')
+popdata <- popdata[c(2,4)]
+names(popdata) <- c('country', 'population')
+popdata$population <- as.numeric(gsub(',', '', popdata$population))
+popdata$country[popdata$country == 'TFYR Macedonia'] <- 'The former Yugoslav Republic of Macedonia'
+europe@data <- merge(europe@data, popdata, by.x='NAME', by.y='country', all.x=TRUE, sort=FALSE)
+europe@data <- europe@data[order(as.integer(as.character(europe@data$SP_ID))),]
+europe$population <- europe$population / 1000000
+europe$population[is.na(europe$population)] <- 0
+shades.pop <- auto.shading(europe$population, n = 8, cutter = rangeCuts, cols = brewer.pal(8, "RdYlBu"))
+choropleth(europe, europe$population, shades.pop)
+choro.legend(45, 80, shades.pop, fmt="%4.0f", title = "Population (Million)", 
+             cex = 0.75, inset = 0.15)
+title('Populations across Europe')
+
+# Load map for German federal states/counties from shapefile
+counties <- readShapePoly('data/bld', proj4string=p4str)
+counties$GEN <- as.character(counties$GEN)
+
+# Load our sales data generated in Dynamic2.R :-)
+load("data/_sales_all.Rdata")
+
+# Aggregates for counties
+sales.county <- ddply(sales.all, c('County'), summarize, Sales=sum(Amount)/1000000)
+sales.county$County <- as.character(sales.county$County)
+counties@data <- merge(counties@data, sales.county, by.x='GEN', by.y='County', all.x=TRUE, sort=FALSE)
+shades <- auto.shading(counties$Sales, n = 5, cutter = rangeCuts, cols = brewer.pal(5, "Blues"))
+
+choropleth(counties, counties$Sales, shades)
+choro.legend(16, 55, shades, title = "Sales (Million €)")
+title('Diesel sales Germany 2015')
+north.arrow(6, 54, 0.2, lab='N')
+
+# rm(list = ls())
