a99a4fe4d873c6286efbe575ce44386f30c725f5
Thomas <SQLThomas@users.noreply.github.com>
2018-11-25 19:10:17 +0100

    Add files via upload

---
+++ Join2018/Tidyverse/05_model.R
@@ -0,0 +1,6 @@
+# 05_model.R
+# Demo script: generate models by the Tidyverse
+# ©2017 Thomas Hütter, this script is provided as-is for demo and educational use only,
+# without warranty of any kind for any other purposes, so run at your own risk!
+
+# Not for this presentation - not a part of the core :-(
